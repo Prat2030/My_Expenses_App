ceece6114abde15770696c213d940d509ba1a00f
Prat2030 <pratyushjindal2003@gmail.com>
2022-04-18 20:26:37 +0530

    change list of transactions design

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -39,44 +39,23 @@ class TransactionList extends StatelessWidget {
           : ListView.builder(
               itemBuilder: ((context, index) {
                 return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                          margin: EdgeInsets.symmetric(
-                            vertical: 10,
-                            horizontal: 15,
-                          ),
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              color: Colors.purple,
-                              width: 2,
-                            ),
-                          ),
-                          padding: EdgeInsets.all(10),
-                          child: Text(
-                            '\₹ ${transactions[index].money.toStringAsFixed(2)}',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Color.fromARGB(255, 63, 63, 60),
-                            ),
-                          )),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            transactions[index].title,
-                            style: Theme.of(context).textTheme.headline6,
-                          ),
-                          Text(
-                            // tx.date.toString(),
-                            DateFormat.yMMMMd()
-                                .format(transactions[index].date),
-                            style: TextStyle(fontSize: 15, color: Colors.grey),
-                          ),
-                        ],
-                      ),
-                    ],
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                        radius: 30,
+                        child: Padding(
+                          padding: EdgeInsets.all(6),
+                          child: FittedBox(
+                              child: Text('\₹${transactions[index].money}')),
+                        )),
+                    title: Text(
+                      transactions[index].title,
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    subtitle: Text(
+                      DateFormat.yMMMd().format(transactions[index].date),
+                    ),
                   ),
                 );
               }),
